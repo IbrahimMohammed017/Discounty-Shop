1d415aa618d41d1ef8d153ae0e79c652bf8dbb1a
Ibrahim Mohammed <Ibrahimmohammed017@gmail.com>
2023-12-26 11:15:09 +0200

    nicescroole

--- js/plugin.js
+++ js/plugin.js
@@ -87,3 +87,12 @@ $(document).ready(function () {
     $(".fa-tiktok").css("color", "#fff");
   }
 });
+
+// Trigger Nice Scroll
+  $("html").niceScroll({
+    cursorcolor: "#fd5825",
+    cursorwidth: "7.5px",
+    cursorborder: "1px solid #ff6200",
+    zindex: "2000",
+  });
+
